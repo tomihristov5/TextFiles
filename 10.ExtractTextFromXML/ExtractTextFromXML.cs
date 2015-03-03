8d9475af7223908929c05af11d7539cd77478a12
tomihristov5 <hrist0v@abv.bg>
2015-03-03 19:34:36 +0200

    homework

---
+++ 10.ExtractTextFromXML/ExtractTextFromXML.cs
@@ -0,0 +1,35 @@
+﻿// Problem 10. Extract text from XML
+// Write a program that extracts from given XML file all the text without the tags.
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ExtractTextFromXML
+{
+    static readonly StringBuilder textWithoutTags = new StringBuilder();
+    static void Main()
+    {
+        const string PATH_TEXT = "../../text.xml";
+
+        ExtractTextWithoutTags(PATH_TEXT);
+
+        Console.WriteLine(textWithoutTags);
+    }
+
+    static void ExtractTextWithoutTags(string pathTextFile)
+    {
+        using (StreamReader reader = new StreamReader(pathTextFile))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = Regex.Replace(reader.ReadLine(), @"<[^>]*>", String.Empty).Trim();
+                if (line != "")
+                {
+                    textWithoutTags.AppendLine(line);
+                }
+            }
+        }
+    }
+}
